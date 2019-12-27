4a03c2024c8fc0f212acb62a81e7517624813fdf
Korisnik <Korisnik@DESKTOP-2CRP2O3>
2019-12-27 14:00:39 +0100

    Add project files.

---
+++ Projekat/FileManagement.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Projekat
+{
+    class FileManagement
+    {
+
+        private string _fileName = null;
+
+        public string FileName
+        {
+            get { return _fileName; }
+            set { this._fileName = value; }
+        }
+
+        public void Store(string LogMessage)
+        {
+            using (StreamWriter fileHandle = new StreamWriter(this._fileName, true))
+            {
+                fileHandle.WriteLine(LogMessage);
+            }
+        }
+
+        public string[] Read()
+        {
+            List<string> listOfNames = new List<string>();
+            using (StreamReader fileHandle = new StreamReader(this._fileName))
+            {
+                string fileContents = "";
+                while ((fileContents = fileHandle.ReadLine()) != null)
+                {
+                    listOfNames.Add(fileContents);
+                }
+            }
+            return listOfNames.ToArray();
+        }
+    }
+}
---
+++ Projekat/KlasaKlasa.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
+
+namespace Projekat
+{
+    class KlasaKlasa : SeleniumBaseClass
+    {
+        [Test]
+        [Category("Vjezba")]
+        public void Test1()
+        {
+            this.NavigateTo("https://www.emmi.rs/");
+            this.DoWait(5);
+            IWebElement monitori = this.FindElement(By.XPath("//a[@title='Monitori']"));
+            monitori.Click();
+            this.DoWait(5);
+            IWebElement brend = this.FindElement(By.Name("brandId"));
+            var select = new SelectElement(brend);
+            select.SelectByValue("448");
+            this.DoWait(5);
+            IWebElement tip = this.FindElement(By.Name("tip"));
+            select = new SelectElement(tip);
+            select.SelectByText("TN");
+            this.DoWait(5);
+            IWebElement trazi = this.FindElement(By.XPath("//input[@value='traži']"));
+            trazi.Click();
+            this.DoWait(5);
+            IWebElement omen = this.FindElement(By.XPath("//a[contains(text(), 'OMEN')]"));
+            omen.Click();
+            this.DoWait(5);
+            IWebElement price = this.FindElement(By.XPath("//div[@class='price']"));
+            var cijena = price.Text.Trim();
+            Assert.AreEqual("29.990", cijena);
+            this.DoWait(3);
+        }
+
+        [SetUp]
+        public void SetUpTests()
+        {
+            //this.Driver = new FirefoxDriver();
+            this.Driver = new ChromeDriver();
+            this.Driver.Manage().Cookies.DeleteAllCookies();
+            this.Driver.Manage().Window.Maximize();
+        }
+
+        [TearDown]
+        public void TearDownTests()
+        {
+            this.Close();
+        }
+    }
+}
